9deed254eff7a84d284ddd6271fe287a3634c9c5
Rwdowan Sultan Rabbey <rsrabbey17@gmail.com>
2025-09-27 19:17:10 +0600

    Operation.java

--- Operation.java
+++ Operation.java
@@ -17,20 +17,37 @@ public class Operation {
     }
 
     // Getters and setters
-    public double getNumOne() { return numOne; }
-    public void setNumOne(double numOne) { this.numOne = numOne; }
+    public double getNumOne() { 
+        return numOne; 
+    }
+    public void setNumOne(double numOne) { 
+        this.numOne = numOne; 
+    }
 
-    public double getNumTwo() { return numTwo; }
-    public void setNumTwo(double numTwo) { this.numTwo = numTwo; }
+    public double getNumTwo() { 
+        return numTwo; 
+    }
+    public void setNumTwo(double numTwo) { 
+        this.numTwo = numTwo; 
+    }
 
-    public String getOperator() { return operator; }
-    public void setOperator(String operator) { this.operator = operator; }
+    public String getOperator() { 
+        return operator; 
+    }
+    public void setOperator(String operator) { 
+        this.operator = operator; 
+    }
 
-    public double getResult() { return result; }
-    public void setResult(double result) { this.result = result; }
+    public double getResult() { 
+        return result; 
+    }
+    public void setResult(double result) { 
+        this.result = result; 
+    }
 
     @Override
     public String toString() {
         return String.format("%.1f %s %.1f = %.1f", numOne, operator, numTwo, result);
     }
+
 }
